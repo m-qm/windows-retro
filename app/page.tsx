b535af37311ec52b53af2bd5cc416c1b4c693466
mireiaquerol <mireiaquerol@gmail.com>
2026-01-21 13:16:37 +0100

    Remove AnimatedCursor component

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@
 
 import { Desktop } from '@/components/Desktop/Desktop';
 import { Taskbar } from '@/components/Desktop/Taskbar';
-import { AnimatedCursor } from '@/components/Cursor/AnimatedCursor';
 
 export default function Home() {
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', position: 'relative' }}>
       <Desktop />
       <Taskbar />
-      <AnimatedCursor />
     </div>
   );
 }
